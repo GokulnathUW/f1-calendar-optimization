38943ce08e3c213741ba15ef61f05a044fb85b21
GokulnathUW <sourirajan@wisc.edu>
2025-11-09 00:14:26 -0600

    Tasks 1.1-1.3

---
+++ src/parameters.py
@@ -0,0 +1,8 @@
+# Emission factors (kg CO2e per tonne-km) – Climatiq
+EMISSION_FACTORS_TONNE_KM = {
+    "air": 0.13516,
+    "land": 0.0165
+}
+
+# Average freight load per team (tonnes)
+AVERAGE_TEAM_FREIGHT_TONNES = 50
