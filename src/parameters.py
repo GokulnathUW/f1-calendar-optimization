7a796f3048bfaf9abb92edf7b886fd6c503678dc
GokulnathUW <sourirajan@wisc.edu>
2025-11-25 11:18:40 -0600

    (ADD) distance and climate calc

--- src/parameters.py
+++ src/parameters.py
@@ -6,3 +6,8 @@ EMISSION_FACTORS_TONNE_KM = {
 
 # Average freight load per team (tonnes)
 AVERAGE_TEAM_FREIGHT_TONNES = 50
+
+# Weather feasibility thresholds
+MIN_TEMP_C = 10      # Minimum ambient temperature for race viability
+MAX_TEMP_C = 35      # Maximum ambient temperature for race viability
+MAX_PRECIP_MM = 40  # Maximum weekly precipitation for race feasibility
