dfbac4297655a159550156b125d6698676c767d7
GokulnathUW <sourirajan@wisc.edu>
2025-12-06 15:21:16 -0600

    Initial model

--- src/parameters.py
+++ src/parameters.py
@@ -8,6 +8,6 @@ EMISSION_FACTORS_TONNE_KM = {
 AVERAGE_TEAM_FREIGHT_TONNES = 50
 
 # Weather feasibility thresholds
-MIN_TEMP_C = 10      # Minimum ambient temperature for race viability
-MAX_TEMP_C = 35      # Maximum ambient temperature for race viability
-MAX_PRECIP_MM = 40  # Maximum weekly precipitation for race feasibility
+MIN_TEMP_F = 50      # Minimum ambient temperature for race viability
+MAX_TEMP_F = 95      # Maximum ambient temperature for race viability
+MAX_PRECIP_IN = 1.57  # Maximum weekly precipitation for race feasibility
